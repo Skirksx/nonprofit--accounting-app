235d4f5e32ecfc56d8e94b57d731241c026cef5f
Skirksx <sbkirk@outlook.com>
2026-07-26 19:44:34 -0400

    Reorder server startup functions

--- server.js
+++ server.js
@@ -1,5 +1,3 @@
 import { startRenderServer } from "./src/renderServer.ts";
 
-await startRenderServer();import { startAccountRegisterServer } from "./src/accountRegisterStandaloneServer.ts";
-
-await startAccountRegisterServer();
+await startRenderServer();
